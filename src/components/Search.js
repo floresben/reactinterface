f1e9343c84a38914fbd16311a82845901c9e5af7
Ben Flores <32658971+floresben@users.noreply.github.com>
2022-04-18 15:57:56 -0400

    test

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,11 +9,10 @@ const Search = () => {
             <label htmlFor="query" className="sr-only" />
             </div>
             <input type="text" name="query" id="query" value=""
-            onChange={(event) => { onQueryChange(event.target.value) }}
             className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
             <div className="absolute inset-y-0 right-0 flex items-center">
             <div>
-                <button type="button" onClick={() => { setToggleSort(!toggleSort) }}
+                <button type="button" 
                 className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                 Sort By <BiCaretDown className="ml-2" />
                 </button>
