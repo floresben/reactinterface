9da6382a4f7387edc0cd2d7dacadc3948a9c40f8
Ben Flores <32658971+floresben@users.noreply.github.com>
2022-04-18 15:45:07 -0400

    Some CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BiArchive } from "react-icons/bi";
 function App() {
   return (
     <div className="App containermx-auto mt-3 font-thin">
-      <h1 className="text-5xl"><BiArchive />Your Appointments</h1>
+      <h1 className="text-5xl"><BiArchive className="inline-block text-red-400"/>Your Appointments</h1>
     </div>
   );
 }
