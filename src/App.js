0b945865f1b12cc64e7a15ed6a34c76594172384
Ben Flores <32658971+floresben@users.noreply.github.com>
2022-04-18 15:53:08 -0400

    added the search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search"
 
 function App() {
   return (
     <div className="App containermx-auto mt-3 font-thin">
-      <h1 className="text-5xl"><BiArchive className="inline-block text-red-400 align-top"/>Your Appointments</h1>
+      <h1 className="text-5xl"><BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
+      <Search />
     </div>
   );
 }
