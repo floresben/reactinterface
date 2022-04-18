455553f7aa6d54086d4c958cf8c1d67cd1232a9b
Ben Flores <32658971+floresben@users.noreply.github.com>
2022-04-18 15:42:56 -0400

    Adding styles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BiArchive } from "react-icons/bi";
 
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive />Your Appointments</h1>
+    <div className="App containermx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><BiArchive />Your Appointments</h1>
     </div>
   );
 }
