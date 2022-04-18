e220efbf7afe849baa7177a8326b6374cf44afd0
Ben Flores <32658971+floresben@users.noreply.github.com>
2022-04-18 15:54:04 -0400

    error handling

--- src/components/Search.js
+++ src/components/Search.js
@@ -17,12 +17,6 @@ const Search = () => {
                 className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                 Sort By <BiCaretDown className="ml-2" />
                 </button>
-                <DropDown toggle={toggleSort}
-                sortBy={sortBy}
-                onSortByChange={mySort => onSortByChange(mySort)}
-                orderBy={orderBy}
-                onOrderByChange={myOrder => onOrderByChange(myOrder)}
-                />
             </div>
             </div>
         </div>
