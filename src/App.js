cc71a0780d3fd5a81379395f369fe3c588c973ce
Ben Flores <32658971+floresben@users.noreply.github.com>
2022-04-19 10:37:19 -0400

    added array

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   let [sortBy, setSortBy] = useState("petName");
   let [orderBy, setOrderBy] = useState("asc");
 
-  const filteredAppointments = [...appointmentList.filter(
+  const filteredAppointments = appointmentList.filter(
     item => {
       return (
         item.petName.toLowerCase().includes(query.toLowerCase()) ||
@@ -25,7 +25,7 @@ function App() {
       a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
         ? -1 * order : 1 * order
     )
-  })]
+  })
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -44,7 +44,7 @@ function App() {
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
       <AddAppointment
-        onSendAppointment={myAppointment => setAppointmentList(...appointmentList, myAppointment)}
+        onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}
         lastId={appointmentList.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
       />
       <Search query={query}
