dc3231f46e5f91425b4d7cf1a476b5f244320a60
Ben Flores <32658971+floresben@users.noreply.github.com>
2022-04-19 09:56:19 -0400

    Added sort

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ function App() {
 
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
 
   const filteredAppointments = appointmentList.filter(
     item => {
-      return(
+      return (
         item.petName.toLowerCase().includes(query.toLowerCase()) ||
         item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
         item.aptNotes.toLowerCase().includes(query.toLowerCase())
       )
     }
-  )
+  ).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order : 1 * order
+    )
+  })
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -29,25 +37,25 @@ function App() {
 
   useEffect(() => {
     fetchData()
-  },[fetchData])
+  }, [fetchData])
 
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
       <AddAppointment />
-      <Search query={query} 
-      onQueryChange={myQuery => setQuery(myQuery)}/>
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)} />
 
       <ul className="divide-y divide-gray-200">
         {filteredAppointments
           .map(appointment => (
-            <AppointmentInfo key={appointment.id} 
-            appointment={appointment} 
-            onDeleteAppointment ={
-              appointmentId =>
-                setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId))
-            } />
+            <AppointmentInfo key={appointment.id}
+              appointment={appointment}
+              onDeleteAppointment={
+                appointmentId =>
+                  setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId))
+              } />
           ))
         }
       </ul>
